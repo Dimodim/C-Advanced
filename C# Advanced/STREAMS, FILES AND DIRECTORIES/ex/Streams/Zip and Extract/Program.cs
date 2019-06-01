d76f92e700c9fa6337887fc0fbc82729f9642e58
DImitar Dimitrov <32570431+Dimodim@users.noreply.github.com>
2019-06-01 16:10:52 +0300

    added more exercises

---
+++ C# Advanced/STREAMS, FILES AND DIRECTORIES/ex/Streams/Zip and Extract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = ".";     //Directory to zip
+            string zipPath = "../../../result.zip"; //zipFile name 
+            string extractPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//ExtractDirectory
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
