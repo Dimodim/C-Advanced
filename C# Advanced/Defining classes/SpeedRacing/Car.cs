510a4f8afa241c3b11d5989a6930b690948c7304
DImitar Dimitrov <32570431+Dimodim@users.noreply.github.com>
2019-06-10 20:24:25 +0300

    added speed racing

---
+++ C# Advanced/Defining classes/SpeedRacing/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public string model;
+        public double fuelAmount;
+        public double fuelConsumption;
+        public double travelledDistance;
+
+        
+        public Car(string model,double fuelAmount,double fuelConsumption)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumption = fuelConsumption;
+            this.travelledDistance = 0;
+        }
+        
+        public  void Drive(double kilometers)
+        {
+
+            if (this.fuelAmount < kilometers * this.fuelConsumption)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.fuelAmount -= kilometers * this.fuelConsumption;
+                this.travelledDistance += kilometers;
+            }
+           
+            
+        }
+
+
+        
+
+
+
+
+
+    }
+}
---
+++ C# Advanced/Defining classes/SpeedRacing/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            Car[] cars = new Car[numberOfCars];
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumption = double.Parse(input[2]);
+                cars[i]=new Car(model,fuelAmount,fuelConsumption);
+            }
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "End")
+                {
+                    break;
+                }
+                else
+                {
+                    string carModel = input[1];
+                    double kmToTravel = double.Parse(input[2]);
+                    cars.Where(c => c.model == carModel).ToList().ForEach(c => c.Drive(kmToTravel));
+                }
+           
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.model} {car.fuelAmount:f2} {car.travelledDistance}");
+            }
+        }
+    }
+}
